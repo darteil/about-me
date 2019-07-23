b4ea55f46967416434614ff7eaf05dd35c126f10
Romanov Yurii <darteil.base@gmail.com>
2019-07-23 15:33:24 +0700

    update config

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,5 +1,5 @@
 class Config {
-  public websocketUrl = 'ws://darteil-projects.ru/socket.io';
+  public websocketUrl = 'ws://darteil-projects.ru:5000/socket.io';
   public apiUrl = 'http://darteil-projects.ru/chat';
 }
 export default new Config();
