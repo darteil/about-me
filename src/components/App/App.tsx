bcdccbdc2e9fe7776d99d2b86e157e823b6e80ca
Romanov Yurii <darteil.base@gmail.com>
2019-09-22 16:12:44 +0700

    add fake loading

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FakeTerminal from '../FakeTerminal';
 import Terminal from '../Terminal';
 import Greeting from '../Terminal/CommandProcessing/ResultCommands/Greeting';
+import FakeLoading from '../FakeLoading';
 import styles from './styles.css';
 
 const App = (): JSX.Element => {
   const [showContent, setShowContent] = useState<boolean>(false);
+  const [showLoading, setShowLoading] = useState<boolean>(true);
+
+  useEffect((): void => {
+    setTimeout((): void => {
+      setShowLoading(false);
+    }, 3000);
+  }, []);
 
   return (
     <div className={styles.app}>
-      <FakeTerminal onDone={setShowContent} />
+      {showLoading && <FakeLoading />}
+      {!showLoading && <FakeTerminal onDone={setShowContent} />}
       {showContent && (
         <>
           <Greeting />
