cec6636b632ecb1c14d644f5717d0ec54d3a7992
Romanov Yuri <darteil.base@gmail.com>
2019-10-31 14:52:54 +0700

    disable feedback

--- src/components/Terminal/CommandProcessing/ResultCommands/Help.tsx
+++ src/components/Terminal/CommandProcessing/ResultCommands/Help.tsx
@@ -5,7 +5,7 @@ const Help = (): JSX.Element => (
     <p>ls - список доступных файлов</p>
     <p>cat [filename] - просмотр файла</p>
     <p>clear - очистка экрана</p>
-    <p>feedback - оставить сообщение</p>
+    {/*<p>feedback - оставить сообщение</p>*/}
   </div>
 );
 
