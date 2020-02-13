b938ca645b08f442f66663851e4aefe965e43b71
Romanov Yuri <darteil.base@gmail.com>
2020-02-13 17:15:40 +0700

    code-split settings

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -7,6 +7,9 @@ const publicPath = path.resolve(__dirname, '../public');
 module.exports = (env, argv) =>
   merge(base(env, argv), {
     devtool: 'source-map',
+    output: {
+      chunkFilename: '[name].chunk.js',
+    },
     devServer: {
       contentBase: publicPath,
       stats: 'errors-only',
