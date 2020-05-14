04467febbcac145b5d9122274eed45a6f1f7f3ee
Romanov Yuri <darteil.base@gmail.com>
2020-05-14 11:46:33 +0700

    update webpack config and dependencies

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -1,14 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const indexFilePath = path.resolve(__dirname, '../src/index.tsx');
 
-const stylesNotIncludedInModules = [/node_modules/, path.resolve(__dirname, '../src/globalStyles/')];
-
 module.exports = (env, argv) => {
-  const devMode = argv.mode !== 'production';
-
   return {
     entry: indexFilePath,
     resolve: {
@@ -22,36 +17,6 @@ module.exports = (env, argv) => {
           use: [
             {
               loader: 'ts-loader',
-            },
-          ],
-        },
-        {
-          test: /\.css$/,
-          exclude: stylesNotIncludedInModules,
-          use: [
-            devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-            {
-              loader: 'css-loader',
-              options: {
-                url: false,
-                importLoaders: 1,
-                localIdentName: '[path][name]__[local]__[hash:base64:5]',
-                modules: true,
-              },
-            },
-          ],
-        },
-        {
-          test: /\.css$/,
-          include: stylesNotIncludedInModules,
-          use: [
-            devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-            {
-              loader: 'css-loader',
-              options: {
-                url: false,
-                importLoaders: 1,
-              },
             },
           ],
         },
