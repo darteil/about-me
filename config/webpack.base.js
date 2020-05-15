6b5c972a9fee32ea7a7b18106aa645ae07c66249
Romanov Yuri <darteil.base@gmail.com>
2020-05-15 14:11:43 +0700

    update webpack config

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -3,31 +3,29 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const indexFilePath = path.resolve(__dirname, '../src/index.tsx');
 
-module.exports = (env, argv) => {
-  return {
-    entry: indexFilePath,
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js'],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(ts|tsx|js)$/,
-          exclude: /(node_modules)/,
-          use: [
-            {
-              loader: 'ts-loader',
-            },
-          ],
-        },
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        inject: true,
-        template: path.resolve(__dirname, '../public/index.html'),
-        filename: 'index.html',
-      }),
+module.exports = {
+  entry: indexFilePath,
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js)$/,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
     ],
-  };
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve(__dirname, '../public/index.html'),
+      filename: 'index.html',
+    }),
+  ],
 };
--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,26 +2,25 @@ const path = require('path');
 const merge = require('webpack-merge');
 const base = require('./webpack.base.js');
 
-const publicPath = path.resolve(__dirname, '../public');
+const publicFolderPath = path.resolve(__dirname, '../public');
 
-module.exports = (env, argv) =>
-  merge(base(env, argv), {
-    devtool: 'source-map',
-    output: {
-      chunkFilename: '[name].chunk.js',
-    },
-    devServer: {
-      contentBase: publicPath,
-      stats: 'errors-only',
-      overlay: true,
-      port: 4000,
-      historyApiFallback: true,
-      proxy: [
-        {
-          context: ['/api'],
-          target: 'http://localhost:3000',
-          pathRewrite: { '^/api': '' },
-        },
-      ],
-    },
-  });
+module.exports = merge(base, {
+  devtool: 'source-map',
+  output: {
+    chunkFilename: '[name].chunk.js',
+  },
+  devServer: {
+    contentBase: publicFolderPath,
+    stats: 'errors-only',
+    overlay: true,
+    port: 4000,
+    historyApiFallback: true,
+    proxy: [
+      {
+        context: ['/api'],
+        target: 'http://localhost:3000',
+        pathRewrite: { '^/api': '' },
+      },
+    ],
+  },
+});
